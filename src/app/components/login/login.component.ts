87df83f0b4743b2faca7f7830d51a42863c6b529
Krzysztof Radwanski <krzysztof.radwanski@pl.abb.com>
2019-05-20 22:07:36 +0200

    working on login and authorization with DB

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+    private _flashMessages: FlashMessagesService
+    ) { }
 
   ngOnInit() {
+    this._authService.getAuth().subscribe(auth =>{
+      if(auth){
+        this._router.navigate(['/']);
+      }
+    });
   }
 
+  onSubmit(): void{
+    this._authService.login(this.email, this.password)
+    .then(() => {
+      this._flashMessages.show('You are now logged in', {cssClass: 'alert-success', timeout: 4000});
+      this._router.navigate(['/']);
+    })
+    .catch(err => {
+      this._flashMessages.show(err.message , {cssClass: 'alert-danger', timeout: 4000});
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private _afAuth: AngularFireAuth) { }
 
-  login(email: string, password: string){
-    // return new Promise(resolve, reject) => {
-    //   this.afAuth.auth.signInWithEmailAndPassword(email, password)
-    //   .then(data)
-    // }
+  login(email: string, password: string)
+  {
+    return new Promise((resolve, reject) => {
+      this._afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then(userData => resolve(userData), err => reject(err));
+    });
+  }
+
+  getAuth(){
+    return this._afAuth.authState.pipe(auth => auth);
   }
 }
