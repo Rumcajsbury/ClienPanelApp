f54932503a05bc3d340d4b021597bcd9032e9bc8
Krzysztof Radwanski <krzysztof.radwanski@pl.abb.com>
2019-05-23 18:49:18 +0200

    added register funcionlity

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _flashMessages: FlashMessagesService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this._authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this._router.navigate(['/']);
+      }
+    });
   }
 
+  onSubmit(): void {
+    this._authService.register(this.email, this.password)
+      .then(() => {
+        this._flashMessages.show('You are now registered and logged', { cssClass: 'alert-success', timeout: 4000 });
+        this._router.navigate(['/']);
+      })
+      .catch(err => {
+        this._flashMessages.show(err.message, { cssClass: 'alert-danger', timeout: 4000 });
+      });
+    }
 }
